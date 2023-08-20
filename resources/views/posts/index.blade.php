0ac38a19177f655dad2c6ae7d4bb3844171844c6
Eding Muhamad <masaruedo@yahoo.com>
2023-08-20 22:56:00 +0700

    Create application layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Blog</title>
-</head>
-<body>
-    <h1>My personal blog</h1>
+@extends('app-layout')
+
+@section('content')
     @if ($posts->count())
-        <ul>
+        <ul class="mt-10 space-y-10">
             @foreach ($posts as $post)
                 <li>
-                    <div>
-                        <h2><a href="/posts/{{ $post->slug }}">{{ $post->slug }}</a></h2>
+                    <div class="space-y-5 prose max-w-none text-justify">
+                        <h2 class="not-prose">
+                            <a class="hover:text-indigo-700 transition-colors duration-100" href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                        </h2>
                         <div>{{ $post->excerpt }}</div>
-                        <div>Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
+                        <div class="text-sm">Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
                     </div>
                 </li>
             @endforeach
         </ul>
     @endif
-</body>
-</html>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Blog</title>
-</head>
-<body>
-    <h1>{{ $post->title }}</h1>
-    <div>Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
-    <div>{{ $post->contents }}</div>
-</body>
-</html>
+@extends('app-layout')
+
+@section('content')
+    <div class="mt-10 space-y-5 prose max-w-none">
+        <h1 class="not-prose">{{ $post->title }}</h1>
+        <div class="text-sm">Posted {{ $post->date->diffForHumans() }} by {{ $post->author }}</div>
+        <div>{{ $post->contents }}</div>
+    </div>
+@endsection
