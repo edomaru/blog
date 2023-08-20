1d855221630357b64db80337480036e11e0d6cf4
Eding Muhamad <masaruedo@yahoo.com>
2023-08-20 16:12:52 +0700

    Create routes and views

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Blog</title>
+</head>
+<body>
+    <h1>My personal blog</h1>
+    <ul>
+        <li><a href="/posts/1">Post 1</a></li>
+        <li><a href="/posts/2">Post 2</a></li>
+    </ul>
+</body>
+</html>
